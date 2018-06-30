4fb06c5a61b7745792019b3ce5e1466e0d83629b
x <imohammedibrahim01@gmail.com>
2018-07-01 00:14:46 +0200

    fix a bug

--- app/src/main/java/com/example/x/tablayout_with_fragments/AppFragmentPagerAdapter.java
+++ app/src/main/java/com/example/x/tablayout_with_fragments/AppFragmentPagerAdapter.java
@@ -5,12 +5,13 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AppFragmentPagerAdapter extends FragmentPagerAdapter {
 
-    private List<Fragment> fragmentList;
-    private List<String> titles;
+    private List<Fragment> fragmentList = new ArrayList<>();
+    private List<String> titles = new ArrayList<>();
 
 
     public AppFragmentPagerAdapter(FragmentManager fm) {
