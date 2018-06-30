c43f61c4fec75cab3dbf759af06edaa306080318
x <imohammedibrahim01@gmail.com>
2018-06-30 23:51:14 +0200

    create fragment classes

---
+++ app/src/main/java/com/example/x/tablayout_with_fragments/AchievementsFragment.java
@@ -0,0 +1,22 @@
+package com.example.x.tablayout_with_fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AchievementsFragment extends Fragment {
+
+    public AchievementsFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_achievements, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/x/tablayout_with_fragments/TasksFragment.java
@@ -0,0 +1,22 @@
+package com.example.x.tablayout_with_fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TasksFragment extends Fragment {
+
+    public TasksFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_tasks, container, false);
+        return view;
+    }
+}
