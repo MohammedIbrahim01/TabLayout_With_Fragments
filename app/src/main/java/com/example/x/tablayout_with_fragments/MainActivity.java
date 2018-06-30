f39457bb9483fd295ee518bce0d8537dcc4419d9
x <imohammedibrahim01@gmail.com>
2018-07-01 00:12:24 +0200

    hookup adapter , tabLayout , viewPager

--- app/src/main/java/com/example/x/tablayout_with_fragments/MainActivity.java
+++ app/src/main/java/com/example/x/tablayout_with_fragments/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.x.tablayout_with_fragments;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        AppFragmentPagerAdapter adapter = new AppFragmentPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new TasksFragment(), "tasks");
+        adapter.addFragment(new AchievementsFragment(), "achievements");
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
