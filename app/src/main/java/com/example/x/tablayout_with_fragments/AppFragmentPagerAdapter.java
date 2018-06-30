85d777f78fd6983a9514136c83d5bb6e174c8b3e
x <imohammedibrahim01@gmail.com>
2018-07-01 00:04:10 +0200

    add fragmentPagerAdapter

---
+++ app/src/main/java/com/example/x/tablayout_with_fragments/AppFragmentPagerAdapter.java
@@ -0,0 +1,40 @@
+package com.example.x.tablayout_with_fragments;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+public class AppFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    private List<Fragment> fragmentList;
+    private List<String> titles;
+
+
+    public AppFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return titles.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titles.get(position);
+    }
+
+    public void addFragment(Fragment fragment, String title){
+        fragmentList.add(fragment);
+        titles.add(title);
+    }
+}
